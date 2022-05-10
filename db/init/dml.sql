afe11ee002a78eb2188c5397d84b10f627dfb2d2
shou-watanabe <watanabeshou1520@gmail.com>
2022-05-10 11:19:15 +0900

    :sparkles: #12 add user dummy data

--- db/init/dml.sql
+++ db/init/dml.sql
@@ -3,7 +3,24 @@ use 42game;
 INSERT INTO
     users (name, token, high_score, coin)
 VALUES
-    ("Amy", UUID(), 0, 0);
+    ("Amy", UUID(), 50, 10),
+    ("Bob", UUID(), 60, 42),
+    ("Cate", UUID(), 100, 30),
+    ("Dian", UUID(), 30, 40),
+    ("Ema", UUID(), 70, 50),
+    ("Finn", UUID(), 40, 70),
+    ("Genie", UUID(), 30, 60),
+    ("Hello", UUID(), 60, 1000),
+    ("Iron", UUID(), 90, 40),
+    ("Jack", UUID(), 10, 660),
+    ("Ken", UUID(), 30, 770),
+    ("Len", UUID(), 30, 20),
+    ("Mikele", UUID(), 90, 0),
+    ("Nick", UUID(), 90, 340),
+    ("Oruga", UUID(), 40, 420),
+    ("Prin", UUID(), 30, 210),
+    ("Queen", UUID(), 50, 720),
+    ("Reck", UUID(), 80, 90);
 
 INSERT INTO
     user_character_possessions (user_id, character_id)
